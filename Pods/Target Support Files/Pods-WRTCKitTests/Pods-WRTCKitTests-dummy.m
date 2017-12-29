ce88c54d56a3dd8660a98144d8d83d676b79aaa6
medabida <medabida2428@gmail.com>
2017-12-29 21:20:38 +0100

    Initial commit

---
+++ Pods/Target Support Files/Pods-WRTCKitTests/Pods-WRTCKitTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WRTCKitTests : NSObject
+@end
+@implementation PodsDummy_Pods_WRTCKitTests
+@end
---
+++ WRTCKit/WRTCKit.h
@@ -0,0 +1,19 @@
+//
+//  WRTCKit.h
+//  WRTCKit
+//
+//  Created by Med on 12/29/17.
+//  Copyright © 2017 Med Abida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WRTCKit.
+FOUNDATION_EXPORT double WRTCKitVersionNumber;
+
+//! Project version string for WRTCKit.
+FOUNDATION_EXPORT const unsigned char WRTCKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WRTCKit/PublicHeader.h>
+
+
